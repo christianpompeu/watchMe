53e1f4086a73d83cd0bfc90ed183311d85711b10
Christian <christianpompeu@gmail.com>
2021-12-02 17:35:25 -0300

    Testing the best way

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,14 @@
-export function Content() {
-  // Complete aqui
+import { Header } from "./Header";
+import "../styles/content.scss";
+
+interface ContentProps{
+  title: string
+}
+
+export function Content(props: ContentProps) {
+  return(
+    <div className="container">
+      <Header title={props.title}/>
+    </div>
+  )
 }
